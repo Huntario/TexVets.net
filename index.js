abdfd78f35545f328d9fa49a52c192f3fc8ba3e7
Hunter Leachman <leachmanh@MacBook-Air-2.local>
2017-01-25 17:23:25 -0600

    got express loading static files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-
 
 io.on('connection', function(socket){
    	socket.on('chat message', function(msg){
