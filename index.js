8df7652e5bccb2461d3692477030b03ad09ba0ee
Damon Foster <Damon Foster>
2017-02-06 21:26:59 -0600

    connection to mlab and local host

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ var Schools = require("./models/Schools.js");
 var Jerbs = require("./models/Jerbs.js");
 var Clinics = require("./models/Clinics.js");
 var Social = require("./models/Social.js");
+var mongoose = require("mongoose");
 
+var mongoUri;
+if (process.argv[2] === 'production') {
+	var keys = require("./keys.js");
+	mongoUri = keys.mongoURI;
+} else if (process.env.MONGODB_URI) {
+	mongoUri = process.env.MONGODB_URI;
+} else {
+	mongoUri = 'mongodb://localhost:27017/txvetsdb'
+}
+
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
 
 var port = process.env.PORT || 3000;
 
