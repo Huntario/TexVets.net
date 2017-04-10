59dddfda8c973bb5175463f4dd710ca396edfdc3
Hunter Leachman <Leachmanh@gmail.com>
2017-04-10 18:20:44 -0500

    made demo site web ready

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ var Schools = require("./models/Schools.js");
 var Jerbs = require("./models/Jerbs.js");
 var Clinics = require("./models/Clinics.js");
 var Social = require("./models/Social.js");
-var keys = require("./keys.js");
-var mongoose = require("mongoose");
 var _ = require('underscore');
 var fakedata = require('./data.js')
-var mongoUri;
-	if (process.argv[2] === 'production') {
-		var keys = require("./keys.js");
-		mongoUri = keys.mongoURI;
-	} else if (process.env.MONGODB_URI) {
-		mongoUri = process.env.MONGODB_URI;
-	} else {
-		mongoUri = 'mongodb://localhost:27017/txvetsdb'
-	}
-	mongoose.connect(mongoUri);
-var db = mongoose.connection;
+// var mongoUri;
+// 	if (process.argv[2] === 'production') {
+// 		var keys = require("./keys.js");
+// 		mongoUri = keys.mongoURI;
+// 	} else if (process.env.MONGODB_URI) {
+// 		mongoUri = process.env.MONGODB_URI;
+// 	} else {
+// 		mongoUri = 'mongodb://localhost:27017/txvetsdb'
+// 	}
+// 	mongoose.connect(mongoUri);
+// var db = mongoose.connection;
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
@@ -57,8 +55,8 @@ function respondToMessage(message){
 	var m = message;
 	var location = getLocation(m);
 	var category = getCategory(m);
-	var response = "Ok I see you want to learn more about " + category + " infromation in " + location + ". One moment while I get that for you...";
-	return response;
+	//var response = "Ok I see you want to learn more about " + category + " infromation in " + location + ". One moment while I get that for you...";
+	//return response;
 	}
 function responseData(message){
 	var m = message;
@@ -66,12 +64,18 @@ function responseData(message){
 	var category = getCategory(m);
 	var catData = fakedata[category];
 	var finalDataArr = [];
+
+	if (catData !== undefined){
 	for (var i = 0; i < catData.length; i++){
 		if (catData[i].city == location){
 			finalDataArr.push(catData[i]);
 			console.log(finalDataArr);
 			return finalDataArr;
 		}
+	}
+	}
+	else {
+		return undefined;
 	}
 }
 var welcome = "Hello, welcome to TexVets. I'm here to help you get information about transitioning to civilian life in Texas.";
